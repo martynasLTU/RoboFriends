f5d989042dfddc8cd6c504e05f3dc2f4e7b3b24e
martynasLTU <martynas.gomas@gmail.com>
2019-11-26 23:54:28 +0200

    Adding ErrorBoundry and Redux

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'tachyons';
 
 const Card = ({id, name, email}) => {
+    // if(true) throw Error('NOOOO');
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img src={`https://robohash.org/robofriend${id}?200x200`} alt='robot'/>
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-//import { robots } from './robots';
 import './App.css';
+import ErrorBoundry from '../components/ErrorBoundry';
 
 class App extends Component {
     constructor() {
@@ -42,7 +42,9 @@ class App extends Component {
             <h1 className='f1'>RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
             <Scroll>
-                <CardList robots={ filteredRobots }/>
+                <ErrorBoundry>
+                    <CardList robots={ filteredRobots }/>
+                </ErrorBoundry>
             </Scroll>
         </div>
         );
