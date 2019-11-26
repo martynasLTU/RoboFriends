aca3fd59391c86f6267f5c0806f6c14e050346a3
martynasLTU <martynas.gomas@gmail.com>
2019-11-26 23:27:38 +0200

    Updates

---
+++ precache-manifest.d46fba9f558b100997b6831d139ea65a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159ecebf69eb24bf69e359282a1f8932",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "782b8afd905f35e69320",
+    "url": "/RoboFriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "ab40f83c866c571fbbc4",
+    "url": "/RoboFriends/static/css/main.6e551283.chunk.css"
+  },
+  {
+    "revision": "782b8afd905f35e69320",
+    "url": "/RoboFriends/static/js/2.202d99ef.chunk.js"
+  },
+  {
+    "revision": "ab40f83c866c571fbbc4",
+    "url": "/RoboFriends/static/js/main.c76dbef5.chunk.js"
+  },
+  {
+    "revision": "6239f40d0ef81a2fd1e0",
+    "url": "/RoboFriends/static/js/runtime-main.6d5bc549.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
